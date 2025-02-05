98da53436e2533d3fd7a0b866882da0a0c3d6b3f
reigadegr <272268642@qq.com>
2025-02-05 15:54:42 +0800

    feat:使用once_cell全局变量保存tick_per_sec

--- src/cpu_common/process_monitor.rs
+++ src/cpu_common/process_monitor.rs
@@ -19,6 +19,7 @@ use anyhow::Result;
 use flume::{Receiver, Sender};
 use hashbrown::{hash_map::Entry, HashMap};
 use libc::{sysconf, _SC_CLK_TCK};
+use once_cell::sync::Lazy;
 use std::{
     cmp, fs,
     sync::{
@@ -28,6 +29,12 @@ use std::{
     thread,
     time::{Duration, Instant},
 };
+
+static TICK_PER_SEC: Lazy<i64> = Lazy::new(|| unsafe { sysconf(_SC_CLK_TCK) });
+
+fn get_tick_per_sec() -> &'static i64 {
+    &TICK_PER_SEC
+}
 
 #[derive(Debug, Clone, Copy)]
 struct UsageTracker {
@@ -48,9 +55,9 @@ impl UsageTracker {
     }
 
     fn try_calculate(&mut self) -> Result<f64> {
-        let tick_per_sec = unsafe { sysconf(_SC_CLK_TCK) };
+        let tick_per_sec = get_tick_per_sec();
         let new_cputime = get_thread_cpu_time(self.pid, self.tid)?;
-        let elapsed_ticks = self.read_timer.elapsed().as_secs_f64() * tick_per_sec as f64;
+        let elapsed_ticks = self.read_timer.elapsed().as_secs_f64() * *tick_per_sec as f64;
         self.read_timer = Instant::now();
         let cputime_slice = new_cputime - self.last_cputime;
         self.last_cputime = new_cputime;
@@ -147,8 +154,9 @@ fn monitor_thread(
                             Some((*tid, tracker.clone().try_calculate().ok()?))
                         })
                         .collect();
-                    top_threads
-                        .sort_unstable_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(cmp::Ordering::Equal));
+                    top_threads.sort_unstable_by(|(_, a), (_, b)| {
+                        b.partial_cmp(a).unwrap_or(cmp::Ordering::Equal)
+                    });
                     top_threads.truncate(5);
                     top_trackers = top_threads
                         .into_iter()
